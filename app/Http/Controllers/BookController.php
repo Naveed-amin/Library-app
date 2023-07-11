fb22c317876739c98ef83b3bad911b33cbf03c52
Naveed  Mughal <naveedmughal1182@gmail.com>
2023-07-11 09:49:54 +0500

    Laravel Library Management System

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index(): View {
+        return view('books.index', ['books' => Book::all()]);
+    }
+}
